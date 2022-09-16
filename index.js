7868e45d07c078c5ff0243932098768a68226cc5
Charity <umercy001@gmail.com>
2022-09-16 18:18:34 +0100

    did some styling

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ buttons.addEventListener('click', function() {
   if (buttons.innerHTML === "Booked"){
     buttons.innerHTML = "Free"
     buttons.style.backgroundColor='white'
+    buttons.style.fontSize='18px'
   } else {
     buttons.innerHTML = "Booked"
     buttons.style.backgroundColor='purple';
+    buttons.style.fontSize='10px'
    
   }
 })
