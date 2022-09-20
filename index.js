38772f89e3aa4156a18c33defeb90c7981a9b399
Charity <umercy001@gmail.com>
2022-09-20 10:37:44 +0100

    Added Booking page

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const buttons = document.getElementById("btn")
+const buttons = document.getElementById('btn')
+const Book = document.getElementById("bookings")
 
 
 buttons.addEventListener('click', function() {
   if (buttons.innerHTML === "Booked"){
     buttons.innerHTML = "Free"
-    buttons.style.backgroundColor='white'
+    buttons.style.backgroundColor ='white'
     buttons.style.fontSize='18px'
+    window.alert('Have a nice drive... We hope to see you again')
   } else {
     buttons.innerHTML = "Booked"
     buttons.style.backgroundColor='purple';
     buttons.style.fontSize='10px'
+    window.alert("You've booked a space for 2hours")
    
   }
 })
-
