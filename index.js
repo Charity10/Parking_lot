d7c33149fecfbdd62225e1b21472ded1d46aac28
Charity <umercy001@gmail.com>
2022-09-22 22:22:31 +0100

    Finaly fixed the button issues

--- index.js
+++ index.js
@@ -1,76 +1,99 @@
-const button1 = document.getElementById('btn1')
-const button2 = document.getElementById('btn2')
-const button3 = document.getElementById('btn3')
-const button4 = document.getElementById('btn4')
-const button5 = document.getElementById('btn5')
-const button6 = document.getElementById('btn6')
-const button7 = document.getElementById('btn7')
-const button8 = document.getElementById('btn8')
-const button9 = document.getElementById('btn9')
-const button10 = document.getElementById('btn10')
-const button11 = document.getElementById('btn11')
-const button12 = document.getElementById('btn12')
-const button13 = document.getElementById('btn13')
-const button14 = document.getElementById('btn14')
-const button15 = document.getElementById('btn15')
-const button16 = document.getElementById('btn16')
-const button17 = document.getElementById('btn17')
-const button18 = document.getElementById('btn18')
-const button19 = document.getElementById('btn19')
-const button20 = document.getElementById('btn20')
 
-const userInfo = document.getElementsById("submit")
-// const buttons = document.querySelectorAll('.packing')
-// const buttons = document.querySelectorAll("div.parking > button")
+// const userInfo = document.getElementsById("submit")
+const submit = document.querySelector('#submit')
+const buttons = document.querySelectorAll('#btn')
 
 
-button1.addEventListener('click', function() {
-  if (button1.innerHTML === "Booked"){
-    button1.innerHTML = "Free"
-    button1.style.backgroundColor ='white'
-    button1.style.fontSize='18px'
-    window.alert('Have a nice drive... We hope to see you again')
-  } else {
-    `${<a href = "parking.html"></a>}`
-    button1.innerHTML = "Booked"
-    button1.style.backgroundColor='purple';
-    button1.style.fontSize='10px'
-    window.alert("You've booked a space for 2hours")
+// button1.addEventListener('click', function() {
+//   if (button1.innerHTML === "Booked"){
+//     button1.innerHTML = "Free"
+//     button1.style.backgroundColor ='white'
+//     button1.style.fontSize='18px'
+//     window.alert('Have a nice drive... We hope to see you again')
+//   } else {
+//     // `${<a href = "parking.html"></a>}`
+//     button1.innerHTML = "Booked"
+//     button1.style.backgroundColor='purple';
+//     button1.style.fontSize='10px'
+//     window.alert("You've booked a space for 2hours")
    
-  }
-})
+//   }
+// })
 
 
-userInfo.addEventListener('click', function() {
-    if()
-})
+// // userInfo.addEventListener('click', function() {
+    
+// // })
 
-button2.addEventListener('click', function() {
-  if (button2.innerHTML === "Booked"){
-    button2.innerHTML = "Free"
-    button2.style.backgroundColor ='white'
-    button2.style.fontSize='18px'
-    window.alert('Have a nice drive... We hope to see you again')
-  } else {
-    button2.innerHTML = "Booked"
-    button2.style.backgroundColor='purple';
-    button2.style.fontSize='10px'
-    window.alert("You've booked a space for 2hours")
+// button2.addEventListener('click', function() {
+//   if (button2.innerHTML === "Booked"){
+//     button2.innerHTML = "Free"
+//     button2.style.backgroundColor ='white'
+//     button2.style.fontSize='18px'
+//     window.alert('Have a nice drive... We hope to see you again')
+//   } else {
+//     button2.innerHTML = "Booked"
+//     button2.style.backgroundColor='purple';
+//     button2.style.fontSize='10px'
+//     window.alert("You've booked a space for 2hours")
    
-  }
+//   }
+// })
+submit.addEventListener('click', function() {
+  localStorage.setItem()
 })
 
-button3.addEventListener('click', function() {
-  if (button3.innerHTML === "Booked"){
-    button3.innerHTML = "Free"
-    button3.style.backgroundColor ='white'
-    button3.style.fontSize='18px'
-    window.alert('Have a nice drive... We hope to see you again')
-  } else {
-    button3.innerHTML = "Booked"
-    button3.style.backgroundColor='purple';
-    button3.style.fontSize='10px'
-    window.alert("You've booked a space for 2hours")
+// button3.addEventListener('click', function() {
+//   if (button3.innerHTML === "Booked"){
+//     button3.innerHTML = "Free"
+//     button3.style.backgroundColor ='white'
+//     button3.style.fontSize='18px'
+//     Submit()
+//   } else {
+//     button3.innerHTML = "Booked"
+//     button3.style.backgroundColor='purple';
+//     button3.style.fontSize='10px'
+//     window.alert("You've booked a space for 2hours")
    
+//   }
+// })
+
+
+
+// button4.addEventListener('click', function() {
+//   if (button4.innerHTML === "Booked"){
+//     button4.innerHTML = "Free"
+//     button4.style.backgroundColor ='white'
+//     button4.style.fontSize='18px'
+//     window.alert('Have a nice drive... We hope to see you again')
+//   } else {
+//     // `${<a href = "parking.html"></a>}`
+//     button4.innerHTML = "Booked"
+//     button4.style.backgroundColor='purple';
+//     button4.style.fontSize='10px'
+//     window.alert("You've booked a space for 2hours")
+   
+//   }
+// })
+
+
+
+  for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener('click', function() {
+      if (buttons[i].innerHTML === "Booked"){
+        buttons[i].innerHTML = "Free"
+        buttons[i].style.backgroundColor ='white'
+        buttons[i].style.fontSize='18px'
+        window.alert('Have a nice drive... We hope to see you again')
+      } else {
+        // `${<a href = "parking.html"></a>}`
+        buttons[i].innerHTML = "Booked"
+        buttons[i].style.backgroundColor='purple';
+        buttons[i].style.fontSize='10px'
+        window.alert("You've booked a space for 2hours")
+       
+      }
+    })
+
   }
-})
